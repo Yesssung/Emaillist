ff96b874bcc3976d864be433be21a183633e9f1d
PARK YESUNG <cjsrnr0504@naver.com>
2024-07-15 10:45:38 +0900

    24.07.15 학원

--- src/main/java/himedia/dao/EmailVo.java
+++ src/main/java/himedia/dao/EmailVo.java
@@ -2,43 +2,40 @@ package himedia.dao;
 
 import java.util.Date;
 
-// Table emaillist의 레코드를 저장하기 위한 DTO
-// 1. 기본생성자를 가지고 있다
-// 2. 비즈니스 로직을 가지지 않은 순수 데이터 객체
-// 3. 필드, getters/setters
-// 4. toString, equals 등 객체 관련 method를 Overide
+//	Table emaillist의 레코드를 저장하기 위한 DTO 객체
+//	1. 기본 생성자를 가지고 있다
+//	2. 비즈니스 로직을 가지지 않은 순수 데이터 객체
+//	3. 필드, getters/setters
+//	4. toString, equals 등 객체 관련 메서드들 오버라이드 
 public class EmailVo {
-	
 	// 필드
 	private Long no;
 	private String lastName;
 	private String firstName;
 	private String email;
 	private Date createdAt;
-	
-	// 기본 생성자
+
+	// 생성자
 	public EmailVo() {
-		
 	}
 
 	// 전체 필드 생성자
 	public EmailVo(Long no, String lastName, String firstName, String email, Date createdAt) {
-		super();
 		this.no = no;
 		this.lastName = lastName;
 		this.firstName = firstName;
 		this.email = email;
 		this.createdAt = createdAt;
 	}
-	
-	// no(PK) -> sequence, createdAt -> default
+
+	// no(PK) -> 시퀀스, createdAt -> default
 	public EmailVo(String lastName, String firstName, String email) {
 		this.lastName = lastName;
 		this.firstName = firstName;
 		this.email = email;
 	}
-	
-	// getters&setters
+
+	// Getters and Setters
 	public Long getNo() {
 		return no;
 	}
@@ -79,14 +76,10 @@ public class EmailVo {
 		this.createdAt = createdAt;
 	}
 
-	// toString
 	@Override
 	public String toString() {
 		return "EmailVo [no=" + no + ", lastName=" + lastName + ", firstName=" + firstName + ", email=" + email
 				+ ", createdAt=" + createdAt + "]";
 	}
-	
-	
-
 
 }
